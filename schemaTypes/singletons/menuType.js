70ff99df3c7de6c9e03b56b7e940b400b23b8fbd
alexander gubernikoff <agubernikoff@gmail.com>
2025-09-10 21:30:37 -0400

    title

--- schemaTypes/singletons/menuType.js
+++ schemaTypes/singletons/menuType.js
@@ -1,20 +1,25 @@
-import {UlistIcon} from '@sanity/icons'
 import {defineField, defineType} from 'sanity'
+import {UlistIcon} from '@sanity/icons'
 
-export default {
+export default defineType({
   name: 'menuPage',
   title: 'Menu',
   type: 'document',
   icon: () => '📋',
   fields: [
     defineField({name: 'defaultImage', type: 'image'}),
-    defineField({
-      name: 'bagels',
-      type: 'bagels',
-    }),
+    defineField({name: 'bagels', type: 'bagels'}),
     defineField({name: 'fish', type: 'array', of: [{type: 'menuItem'}]}),
     defineField({name: 'spreads', type: 'array', of: [{type: 'menuItem'}]}),
     defineField({name: 'sandwiches', type: 'array', of: [{type: 'menuItem'}]}),
     defineField({name: 'drinks', type: 'array', of: [{type: 'menuItem'}]}),
   ],
-}
+  preview: {
+    prepare() {
+      return {
+        title: 'Menu', // always show "Menu"
+        media: UlistIcon,
+      }
+    },
+  },
+})
